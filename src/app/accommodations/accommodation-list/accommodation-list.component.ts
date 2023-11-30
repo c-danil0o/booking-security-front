486034879cc08aaf6a14767d5cb30e4beee3fd15
markostojanovic02 <markostojanovic482@gmail.com>
2023-11-30 13:18:56 +0100

    Created pagination for list of accommodations

--- src/app/accommodations/accommodation-list/accommodation-list.component.ts
+++ src/app/accommodations/accommodation-list/accommodation-list.component.ts
@@ -11,4 +11,28 @@ import { Accommodation } from '../accommodation';
 })
 export class AccommodationListComponent {
   accommodations: Array<Accommodation> = accommodationData;
+
+  currentPage: number = 1;
+  itemsPerPage: number = 3;
+  totalItems: number = this.accommodations.length;
+
+  get totalPages(): number { 
+    return Math.ceil(this.totalItems / this.itemsPerPage);
+  }
+
+  get paginatedAccommodations(): Array<Accommodation> {
+    return this.accommodations.slice((this.currentPage - 1) * this.itemsPerPage, this.currentPage * this.itemsPerPage);
+  }
+
+  changePage(page: number): void {
+    this.currentPage = page;
+  }
+
+  isActive(pageNumber: number): boolean {
+    return this.currentPage === pageNumber;
+  }
+
+  totalPagesArray(): number[] {
+    return Array(this.totalPages).fill(0).map((_, index) => index + 1);
+ }
 }
