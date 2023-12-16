0f9f18ff3aeede8f9367c6b26f02d38b23b9cdc2
danilo_c <danilo.cvijetic10@protonmail.ch>
2023-12-16 17:50:39 +0100

    feat: added redirect after bad jtw token

--- src/app/infrastructure/auth/interceptor.ts
+++ src/app/infrastructure/auth/interceptor.ts
@@ -1,29 +1,53 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 import {
   HttpRequest,
   HttpHandler,
   HttpEvent,
   HttpInterceptor,
 } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import {catchError, Observable, throwError} from 'rxjs';
+import {AuthService} from "./auth.service";
+import {Router} from "@angular/router";
 
 @Injectable()
 export class Interceptor implements HttpInterceptor {
+  constructor(private authService: AuthService, private router: Router) {
+  }
+
   intercept(
     req: HttpRequest<any>,
     next: HttpHandler
   ): Observable<HttpEvent<any>> {
+    console.log("intercepting: ", req)
     const accessToken: any = localStorage.getItem('user');
     if (req.headers.get('skip')) return next.handle(req);
 
     if (accessToken) {
-      const cloned = req.clone({
+      req = req.clone({
         headers: req.headers.set('Authorization', 'Bearer ' + accessToken),
       });
-
-      return next.handle(cloned);
-    } else {
-      return next.handle(req);
+      console.log("added access token")
     }
+    return next.handle(req).pipe(catchError(err => {
+      console.log("checking for errors!", err)
+      if (err.status === 401) {
+        this.logOut()
+      }
+      const error = err.error.message || err.statusText;
+      return throwError(error)
+    }));
+  }
+
+
+  logOut()
+    :
+    void {
+    this.authService.logout().subscribe({
+      next: (_) => {
+        localStorage.removeItem('user');
+        this.authService.setUser();
+        this.router.navigate(['/login']);
+      }
+    })
   }
 }
