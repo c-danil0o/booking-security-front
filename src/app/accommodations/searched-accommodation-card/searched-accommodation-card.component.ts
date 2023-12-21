d2966ef52bb90a3e032f31630c020abde93e064d
danilo_c <danilo.cvijetic10@protonmail.ch>
2023-12-21 01:07:51 +0100

    fix: some minor fixes for profiles

--- src/app/accommodations/searched-accommodation-card/searched-accommodation-card.component.ts
+++ src/app/accommodations/searched-accommodation-card/searched-accommodation-card.component.ts
@@ -1,6 +1,7 @@
 import {Component, EventEmitter, Input, Output} from '@angular/core';
 import {Accommodation} from "../../model/accommodation-model";
 import {SearchedAccommodation} from "../../model/searched-accommodation-model";
+import {environment} from "../../../env/env";
 
 @Component({
   selector: 'app-searched-accommodation-card',
@@ -18,8 +19,8 @@ export class SearchedAccommodationCardComponent {
     console.log(this.accommodation.photos)
   }
 
-  getPhotoURI(): string[]{
-    return this.accommodation.photos.map(element => '../../../../../assets/' + element)
+  getPhotoURI(): string{
+    return environment.filesApi + this.accommodation.photos[0]
     //return this.accommodation.photos[0]
   }
 }
