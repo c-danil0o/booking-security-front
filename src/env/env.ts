284dbc17fa81be9b1ecf191e26a0494f8b9538ec
cornenkiV <vladimircornenki02@gmail.com>
2024-04-22 02:01:09 +0200

    feat: https configured ssl

--- src/env/env.ts
+++ src/env/env.ts
@@ -1,6 +1,6 @@
 export const environment = {
-  apiHost: 'http://localhost:8080/',
-  socket: 'http://localhost:8080/socket',
+  apiHost: 'https://localhost:8085/',
+  socket: 'https://localhost:8085/socket',
   googleMapsApi: "AIzaSyA-7iAwTLpwQJRR21oeE9y15zW3QVd6I-8&libraries=places",
-  filesApi: 'http://localhost:8080/files/'
+  filesApi: 'https://localhost:8085/files/'
 }
