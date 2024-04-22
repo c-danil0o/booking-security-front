2d26451621c18347cdff69af66cbb27ce4c6f3ea
cornenkiV <vladimircornenki02@gmail.com>
2024-04-22 02:08:20 +0200

    fix: fixed ports

--- src/env/env.ts
+++ src/env/env.ts
@@ -1,6 +1,6 @@
 export const environment = {
-  apiHost: 'https://localhost:8085/',
-  socket: 'https://localhost:8085/socket',
+  apiHost: 'https://localhost:8080/',
+  socket: 'https://localhost:8080/socket',
   googleMapsApi: "AIzaSyA-7iAwTLpwQJRR21oeE9y15zW3QVd6I-8&libraries=places",
-  filesApi: 'https://localhost:8085/files/'
+  filesApi: 'https://localhost:8080/files/'
 }
